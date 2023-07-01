9c1fc0434b53c79fc1d7c004f693fefbec792789
MartaCzM <>
2023-07-01 07:59:09 +0000

    Add code for the questions in javascript

---
+++ assets/game.js
@@ -0,0 +1,22 @@
+const scoreDisplay = document.getElementById(score-display)
+const questionDisplay = document.getElementById(question-display)
+
+const questions = [
+    {
+        quiz: ['value', 'estimate', 'evaluate'],
+        options: ['jusry', 'assess'],
+        correct: 2
+    },
+
+    {
+        quiz: ['close', 'near', 'next'],
+        options: ['adjacent', 'trace'],
+        correct: 1
+    },
+
+    {
+        quiz: ['foreign', 'national', 'ethnic'],
+        options: ['exotic', 'unknown'],
+        correct: 1
+    },
+]
