1799f84e9dc18c6d3501fcf7932a998ef290d91f
MartaCzM <>
2023-07-06 14:34:11 +0000

    Add menu buttons and style

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+const synonymQuestionDisplay = document.getElementById('synonym-question-display')
+
+const question = [
+  {
+    quiz: ['value', 'estimate', 'evaluate'], 
+    option: ['jury', 'assess'],
+    correct: 2,
+  },
+  {
+    quiz: ['close', 'near', 'next'],
+    option: ['trace', 'adjacent'],
+    correct: 2,
+  },
+  {
+    quiz: ['foreign', 'national', 'ethnic'],
+    option: ['mad', 'exotic'],
+    correct: 2,
+  },
+  {
+    quiz: ['assume', 'insight', 'weather'],
+    option: ['forecast', 'sustainable'],
+    correct: 1,
+  },
+  {
+    quiz: ['fast', 'quick', 'prompt'],
+    option: ['charity', 'rapid'],
+    correct: 2,
+  },
+]
