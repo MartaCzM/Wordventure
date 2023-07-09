5390af10dcce502ba6bf6fafe3d5db619434187b
MartaCzM <>
2023-07-09 22:40:33 +0000

    Add the synonym game questions and answers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,40 @@
-const synonymQuestionDisplay = document.getElementById('synonym-question-display')
-
-const question = [
-  {
-    quiz: ['value', 'estimate', 'evaluate'], 
-    option: ['jury', 'assess'],
-    correct: 2,
-  },
-  {
-    quiz: ['close', 'near', 'next'],
-    option: ['trace', 'adjacent'],
-    correct: 2,
-  },
-  {
-    quiz: ['foreign', 'national', 'ethnic'],
-    option: ['mad', 'exotic'],
-    correct: 2,
-  },
-  {
-    quiz: ['assume', 'insight', 'weather'],
-    option: ['forecast', 'sustainable'],
-    correct: 1,
-  },
-  {
-    quiz: ['fast', 'quick', 'prompt'],
-    option: ['charity', 'rapid'],
-    correct: 2,
-  },
-]
+/// Synonym game questions and answers
+const questions = [
+    {
+      question: "Big",
+      options: ["Large", "Small", "Tall"],
+      answer: "Large"
+    },
+    {
+      question: "Happy",
+      options: ["Sad", "Joyful", "Angry"],
+      answer: "Joyful"
+    },
+    {
+      question: "Fast",
+      options: ["Slow", "Quick", "Swift"],
+      answer: "Quick"
+    },
+    {
+      question: "Beautiful",
+      options: ["Ugly", "Attractive", "Gorgeous"],
+      answer: "Gorgeous"
+    },
+    {
+      question: "Brave",
+      options: ["Outrageous", "Valorous", "Courageous"],
+      answer: "Courageous"
+    },
+    {
+      question: "Difficult",
+      options: ["Versatile", "Particular", "Complex"],
+      answer: "Complex"
+    },
+    {
+      question: "Intelligent",
+      options: ["Clever", "Quick", "Agile"],
+      answer: "Clever"
+    }
+  ];
+  
+ 
